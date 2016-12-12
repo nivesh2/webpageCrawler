5b0fba81c7c954772b11cf744ad2cb1602cca476
Nivesh Singh <nivesh2@gmail.com>
2016-12-13 02:41:13 +0530

    getters and setters created for depth

--- global.js
+++ global.js
@@ -1,5 +1,16 @@
 'use strict';
 
+//depth is setted by User during program run
+var _depth=0;
+
+function setDepth(depth){
+    _depth = depth;
+}
+
+function getDepth(){
+    return _depth;
+}
+
 /*
  * depth: defines the depth of the crawler
  * arr: is the global array list which contains the list of all the links
@@ -7,8 +18,10 @@
  * filepath: path and name of the csv file to be created
  * instances: number of concurrent processes to be created to crawl the url
  */
+
 module.exports = exports = {
-    depth: 2,
+    getDepth: getDepth,
+    setDepth:setDepth,
     arr: [],
     url: 'https://medium.com',
     filepath: './output/medium.csv',
